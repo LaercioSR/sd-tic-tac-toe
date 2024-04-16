93e86c9b1528f13647c63c04089afc2e62d50227
Laercio Rios <contact@laerciorios.com>
2024-04-16 19:12:54 -0300

    feat: add mouse input

--- tictactoe.c
+++ tictactoe.c
@@ -1,24 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
-// #include <stdint.h>
-// #include <sys/time.h>
+#include <fcntl.h>
+#include <stdint.h>
+#include <sys/time.h>
 #include "draw.h"
 
 #define SIZE_BOARD 3
 
-// #define EV_KEY 0x01
-// #define EV_REL 0x02
+#define EV_KEY 0x01
+#define EV_REL 0x02
 
-// #define REL_X 0x00
-// #define REL_Y 0x01
+#define REL_X 0x00
+#define REL_Y 0x01
 
-// struct input_event
-// {
-//     struct timeval time;
-//     uint16_t type;
-//     uint16_t code;
-//     int32_t value;
-// };
+struct input_event
+{
+    struct timeval time;
+    uint16_t type;
+    uint16_t code;
+    int32_t value;
+};
 
 int checkWin(char board[SIZE_BOARD][SIZE_BOARD], char player)
 {
@@ -70,39 +71,115 @@ void waitPlay(int player, int *row, int *col)
     } while (!confirmation);
 }
 
+void changePosition(char board[SIZE_BOARD][SIZE_BOARD], int row, int col, int lastRow, int lastCol)
+{
+    cleanBlock(lastRow, lastCol);
+    drawBlock(row, col);
+    if (board[row][col] == 'X')
+    {
+        drawX(row, col);
+    }
+    else if (board[row][col] == 'O')
+    {
+        drawO(row, col);
+    }
+}
+
 int main(void)
 {
+    int fileInput;
+    struct input_event event;
     char board[SIZE_BOARD][SIZE_BOARD] = {
         {' ', ' ', ' '},
         {' ', ' ', ' '},
         {' ', ' ', ' '}};
     char playerValue;
-    int numPlays = 0, row, col;
+    int numPlays = 0, row = 0, col = 0, qttClick = 0;
     int isWinner = 0;
+    int x = 1;
+    int y = 1;
+
+    fileInput = open("/dev/input/event14", O_RDONLY);
+    if (fileInput == -1)
+    {
+        perror("Erro ao abrir o dispositivo de entrada");
+        return 1;
+    }
 
     drawBoard();
     while ((numPlays < 9) && !isWinner)
     {
         int player = numPlays % 2 + 1;
-        waitPlay(player, &row, &col);
 
-        playerValue = player == 1 ? 'X' : 'O';
-        board[row][col] = playerValue;
-
-        if (player == 1)
+        ssize_t bytes = read(fileInput, &event, sizeof(event));
+        if (bytes == -1)
         {
-            drawX(row, col);
+            perror("Erro ao ler o evento");
+            close(fileInput);
+            return 1;
         }
-        else
+        if (event.type == EV_REL && event.code == REL_X)
         {
-            drawO(row, col);
+            if (event.value > 0 && x < 1499)
+            {
+                x++;
+            }
+            else if (event.value < 0 && x > 0)
+            {
+                x--;
+            }
+            if (col != (x / 500))
+            {
+                changePosition(board, row, (x / 500), row, col);
+                col = (x / 500);
+                qttClick = 0;
+            }
         }
+        if (event.type == EV_REL && event.code == REL_Y)
+        {
+            if (event.value > 0 && y < 1499)
+            {
+                y++;
+            }
+            else if (event.value < 0 && y > 0)
+            {
+                y--;
+            }
+            if (row != (y / 500))
+            {
+                changePosition(board, (y / 500), col, row, col);
+                row = (y / 500);
+                qttClick = 0;
+            }
+        }
+        if (event.type == EV_KEY)
+        {
+            qttClick++;
+            if (qttClick == 2)
+            {
+                if (board[row][col] == ' ')
+                {
+                    playerValue = player == 1 ? 'X' : 'O';
+                    board[row][col] = playerValue;
 
-        isWinner = checkWin(board, playerValue);
-        if (isWinner)
-            printf("Player %d is winner!!!\n", player);
+                    if (player == 1)
+                    {
+                        drawX(row, col);
+                    }
+                    else
+                    {
+                        drawO(row, col);
+                    }
 
-        numPlays += 1;
+                    isWinner = checkWin(board, playerValue);
+                    if (isWinner)
+                        printf("Player %d is winner!!!\n", player);
+
+                    numPlays += 1;
+                }
+                qttClick = 0;
+            }
+        }
     }
 
     return 0;
